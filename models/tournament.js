77947bd0a147d7c962e228f0ca93bf9ce07e6436
adam <acarlson1994@gmail.com>
2017-02-20 23:33:43 -0800

    Change id to ObjectId type. Add seeding, type, matchcreationstatus, participants fields.

--- models/tournament.js
+++ models/tournament.js
@@ -7,11 +7,21 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var TournamentSchema   = new Schema({
     title: String,
     description: String,
+    seeding: String,
     private: Boolean,
-    hostsuserid: Number,
-    gameid: Number,
+    type: String,
+    hostsuserid: ObjectId,
+    matchcreationstatus: {
+    	type: String,
+    	default: 'inprogress'
+    },
+    gameid: ObjectId,
+    participants: [{
+		userid: ObjectId,
+		position: Number
+	}],
 	winners: [{
-		userid: Number,
+		userid: ObjectId,
 		name: String,
 		position: Number
 	}]
